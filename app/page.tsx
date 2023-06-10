3d5c086ebaa02273ae3f44dd302f91b1554fd2d2
Eva <17610603221@163.com>
2023-06-11 00:50:30 +0800

    test deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from '@/components/ui/button'
-
 export default function Home() {
-  return (
-    <div className='h-screen flex items-center justify-center'>
-      <Button variant="outline">hello</Button>
-    </div>
-  )
+  return <div className="h-screen flex items-center justify-center">hello</div>
 }
